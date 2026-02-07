ceefb0e59d40a47ac6e658ca0f516f78fdf49c7e
JohanaMisaela <johanna.tahianaharison@gmail.com>
2026-02-07 17:11:33 +0300

    feat: Add Vercel app URL to allowed CORS origins.

--- server.js
+++ server.js
@@ -29,7 +29,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:4200", credentials: true }));
+app.use(
+    cors({
+        origin: ["http://localhost:4200", "https://first-angular-project-lac.vercel.app"],
+        credentials: true,
+    })
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
