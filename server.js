e2cf71b587c92735b564123498152c817950a948
JohanaMisaela <johanna.tahianaharison@gmail.com>
2026-02-07 17:03:23 +0300

    feat: Configure Express to parse JSON and URL-encoded request bodies with a 50MB limit.

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ connectDB();
 const app = express();
 
 app.use(cors({ origin: "http://localhost:4200", credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
